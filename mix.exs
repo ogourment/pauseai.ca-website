64e70bbd86005907aefa431f5de1d02175070a15
Olivier G <ogourment@gmail.com>
2026-08-05 19:52:10 -0400

    Add Montréal events calendar link

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PauseAiCa.MixProject do
   def project do
     [
       app: :pauseai_ca,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
