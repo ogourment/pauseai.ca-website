91e75fdbf20357b10d2a3c8ec3c2a8fc89ef9eb6
Olivier G <ogourment@gmail.com>
2026-08-07 00:00:24 -0400

    Prepare v0.2.16

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PauseAiCa.MixProject do
   def project do
     [
       app: :pauseai_ca,
-      version: "0.2.15",
+      version: "0.2.16",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
